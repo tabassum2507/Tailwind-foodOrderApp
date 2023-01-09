49788b0bb9be0a67d03179f91f5ac404ea246b80
tabassum2507 <10tabassum2@gmail.com>
2023-01-09 23:58:23 +0530

    state

--- src/data/data.js
+++ src/data/data.js
@@ -2,7 +2,22 @@ import { React, useState } from 'react'
 
 const data = () => {
   return (
-    <div>data</div>
+    <div>
+        <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
+      {/* Left side */}
+      <div className='flex items-center'>
+        <div onClick={()=> setNav(!nav)} className='cursor-pointer'>
+          <AiOutlineMenu size={30} />
+        </div>
+        <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>
+          Best <span className='font-bold'>Eats</span>
+        </h1>
+        <div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'>
+          <p className='bg-black text-white rounded-full p-2'>Delivery</p>
+          <p className='p-2'>Pickup</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
